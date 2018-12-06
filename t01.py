bdb81bd8e96b84cbcb702ee2b988f0da128e56c3
hyt <huaiyuat@umich.edu>
2018-12-05 19:59:47 -0500

    add a new file

---
+++ t01.py
@@ -0,0 +1,10 @@
+ Volume in drive C is Local Disk
+ Volume Serial Number is 9841-A8CE
+
+ Directory of C:\Users\thy20\Desktop\test
+
+12/05/2018  07:57 PM    <DIR>          .
+12/05/2018  07:57 PM    <DIR>          ..
+12/05/2018  07:57 PM                 0 t01.py
+               1 File(s)              0 bytes
+               2 Dir(s)  44,807,905,280 bytes free
